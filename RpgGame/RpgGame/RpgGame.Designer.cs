9f3c66e2ca18da828536bd40d5e9f31e917a8bfb
Brandon Ezekiel <807777@ACDM.DS.SAIT.CA>
2019-03-06 17:04:59 -0700

    update

--- RpgGame/RpgGame/RpgGame.Designer.cs
+++ RpgGame/RpgGame/RpgGame.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lvl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lblHitPoints = new System.Windows.Forms.Label();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.lblExp = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(18, 20);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(55, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Hit Points:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(18, 46);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(32, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Gold:";
+            // 
+            // lvl3
+            // 
+            this.lvl3.AutoSize = true;
+            this.lvl3.Location = new System.Drawing.Point(18, 74);
+            this.lvl3.Name = "lvl3";
+            this.lvl3.Size = new System.Drawing.Size(63, 13);
+            this.lvl3.TabIndex = 2;
+            this.lvl3.Text = "Experience:";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(18, 100);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(36, 13);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "Level:";
+            // 
+            // lblHitPoints
+            // 
+            this.lblHitPoints.AutoSize = true;
+            this.lblHitPoints.Location = new System.Drawing.Point(110, 20);
+            this.lblHitPoints.Name = "lblHitPoints";
+            this.lblHitPoints.Size = new System.Drawing.Size(0, 13);
+            this.lblHitPoints.TabIndex = 4;
+            // 
+            // lblGold
+            // 
+            this.lblGold.AutoSize = true;
+            this.lblGold.Location = new System.Drawing.Point(110, 46);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(0, 13);
+            this.lblGold.TabIndex = 5;
+            // 
+            // lblExp
+            // 
+            this.lblExp.AutoSize = true;
+            this.lblExp.Location = new System.Drawing.Point(110, 74);
+            this.lblExp.Name = "lblExp";
+            this.lblExp.Size = new System.Drawing.Size(0, 13);
+            this.lblExp.TabIndex = 6;
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(110, 99);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(0, 13);
+            this.lblLevel.TabIndex = 7;
+            // 
+            // RpgGame
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblExp);
+            this.Controls.Add(this.lblGold);
+            this.Controls.Add(this.lblHitPoints);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lvl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "RpgGame";
+            this.Text = "My Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lvl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lblHitPoints;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.Label lblExp;
+        private System.Windows.Forms.Label lblLevel;
     }
 }
 
--- RpgGame/RpgGame/RpgGame.cs
+++ RpgGame/RpgGame/RpgGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace RpgGame
 {
     public partial class RpgGame : Form
     {
+        private Player _player;
+
         public RpgGame()
         {
             InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaxHitPoints = 10;
+            _player.Gold = 20;
+            _player.Experience = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExp.Text = _player.Experience.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
         }
+
     }
 }
